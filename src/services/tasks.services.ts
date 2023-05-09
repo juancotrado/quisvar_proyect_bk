3cf5b648dd0438b3fc3ca0bc60fc072fdd0be157
unknown <70176266juan@gmail.com>
2023-05-09 11:42:15 -0500

    small change in the order of tasks

--- src/services/tasks.services.ts
+++ src/services/tasks.services.ts
@@ -3,7 +3,9 @@ import { prisma } from '../utils/prisma.server';
 class tasksServices {
   static async getTask() {
     try {
-      const task = await prisma.tasks.findMany();
+      const task = await prisma.tasks.findMany({
+        orderBy: { createdAt: 'desc' },
+      });
       return task;
     } catch (error) {
       throw error;
