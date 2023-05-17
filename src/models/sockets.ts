6e979ecca38bd336c40989a673fb32e170ac807d
Jhon Carlos <gpro1pro@gmail.com>
2023-05-17 17:42:42 -0500

    edit task status and save them in the database

--- src/models/sockets.ts
+++ src/models/sockets.ts
@@ -1,4 +1,5 @@
 import { Server as WebSocketServer } from 'socket.io';
+import { TasksServices } from '../services';
 
 class Sockets {
   private io: WebSocketServer;
@@ -10,8 +11,15 @@ class Sockets {
   socketEvents() {
     this.io.on('connection', socket => {
       console.log('Connect user with id ==>', socket.id);
-      socket.on('data', value => {
-        socket.broadcast.emit('data', value);
+      socket.on('data', tasks => {
+        socket.broadcast.emit('data', tasks);
+      });
+      socket.on('update-status', async data => {
+        const result = await TasksServices.updateStatus(data.id, data.body);
+        console.log('complete update status', result);
+      });
+      socket.on('disconnect', () => {
+        console.log('User disconected ==>', socket.id);
       });
     });
   }
