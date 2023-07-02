aa2b77239f23b3dd933e9e812cc7310649fcde57
juancotrado <jecar324@gmail.com>
2023-07-01 19:49:48 -0500

    Added: description and unique in workareas

--- src/services/workareas.services.ts
+++ src/services/workareas.services.ts
@@ -191,7 +191,7 @@ class WorkAreasServices {
         id,
       },
       include: {
-        project: { select: { name: true, description: true } },
+        project: { select: { name: true, description: true, unique: true } },
         user: {
           include: {
             profile: true,
