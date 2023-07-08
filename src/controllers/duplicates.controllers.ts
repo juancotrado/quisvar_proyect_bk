deaa9751b17372c028d5402b93591889097f77b6
jean ticona <jecar324@gmail.com>
2023-07-08 12:37:37 -0500

    added duplicate route

--- src/controllers/duplicates.controllers.ts
+++ src/controllers/duplicates.controllers.ts
@@ -18,23 +18,23 @@ export const duplicateProject = async (
     const duplicate = await DuplicatesServices.proyect(_project_id);
     const oldPath = await PathServices.pathProject(_project_id);
     const newPath = await PathServices.pathProject(duplicate.id);
-    // if (duplicate) {
-    //   cpSync(oldPath, newPath, { recursive: true });
-    //   cpSync(
-    //     `${_materialPath}/${duplicate.oldName}`,
-    //     `${_materialPath}/${duplicate.name}`,
-    //     {
-    //       recursive: true,
-    //     }
-    //   );
-    //   cpSync(
-    //     `${_reviewPath}/${duplicate.oldName}`,
-    //     `${_reviewPath}/${duplicate.name}`,
-    //     {
-    //       recursive: true,
-    //     }
-    //   );
-    // }
+    if (duplicate) {
+      cpSync(oldPath, newPath, { recursive: true });
+      cpSync(
+        `${_materialPath}/${duplicate.oldName}`,
+        `${_materialPath}/${duplicate.name}`,
+        {
+          recursive: true,
+        }
+      );
+      cpSync(
+        `${_reviewPath}/${duplicate.oldName}`,
+        `${_reviewPath}/${duplicate.name}`,
+        {
+          recursive: true,
+        }
+      );
+    }
     res.status(201).json({ oldPath, newPath });
   } catch (error) {
     next(error);
