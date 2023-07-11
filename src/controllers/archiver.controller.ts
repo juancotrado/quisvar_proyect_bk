3edc5e915ec42b602b49c2ac7dd20fe244d8c50c
Jhon Castillo <jcastillo@dlya.com.uy>
2023-07-10 21:16:06 -0500

    add archiver for route

--- src/controllers/archiver.controller.ts
+++ src/controllers/archiver.controller.ts
@@ -3,24 +3,22 @@ import { PathServices } from '../services';
 import { archiverFolder } from '../utils/archiver';
 import fs from 'fs';
 
+const TYPE_PATH = {
+  routes: PathServices.pathArea,
+  projects: PathServices.pathProject,
+  indextasks: PathServices.pathIndexTask,
+  tasks: PathServices.pathTask,
+  tasks2: PathServices.pathTask2,
+  tasks3: PathServices.pathTask3,
+};
 export const archiverPath = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   try {
-    const TYPE_PATH = {
-      projects: PathServices.pathProject,
-      indextasks: PathServices.pathIndexTask,
-      tasks: PathServices.pathTask,
-      tasks2: PathServices.pathTask2,
-      tasks3: PathServices.pathTask3,
-    };
     const { id } = req.params;
-
     const type = req.query.type as keyof typeof TYPE_PATH;
-
-    console.log(id);
     if (!TYPE_PATH[type]) return;
     const TypePathServices = TYPE_PATH[type];
     const path = await TypePathServices(+id);
