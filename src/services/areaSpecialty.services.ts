02716e51c36eba0aa117eaa46371907c61fe2da0
Jim <j1mer0528@gmail.com>
2023-10-16 21:14:02 -0500

    changed: return area specialty

--- src/services/areaSpecialty.services.ts
+++ src/services/areaSpecialty.services.ts
@@ -1,10 +1,9 @@
+import { log } from 'console';
 import AppError from '../utils/appError';
 import { AreaSpecialty, prisma } from '../utils/prisma.server';
 
 class AreaSpecialtyServices {
   static async createAreaSpecialty(data: AreaSpecialty) {
-    console.log(data);
-
     if (!data) throw new AppError(`Datos incorrectos`, 400);
     const newArea = await prisma.areaSpecialty.create({
       data: {
@@ -21,7 +20,20 @@ class AreaSpecialtyServices {
       where: { specialistId },
     });
 
-    return areas;
+    const newData = Array.from(
+      areas
+        .reduce((map, elem) => {
+          const { specialtyName, ...items } = elem;
+          const oldData = map.get(specialtyName) || {
+            specialtyName,
+            datos: [],
+          };
+          oldData.datos.push(items);
+          return map.set(specialtyName, oldData);
+        }, new Map())
+        .values()
+    );
+    return newData;
   }
 }
 export default AreaSpecialtyServices;
