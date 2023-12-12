337e7a331072118741381bebbd46a016706fba86
Jim <j1mer0528@gmail.com>
2023-12-12 00:16:10 -0500

    change: license return

--- src/services/licenses.services.ts
+++ src/services/licenses.services.ts
@@ -42,35 +42,27 @@ class LicenseServices {
     return updateList;
   }
   static async getLicenceById() {
-    // status: Licenses['status'], // usersId: Licenses['id'],
-    // if (!usersId) throw new AppError('Oops!,ID invalido', 400);
-    // if (!status) throw new AppError('Oops!,Estado incorrecto', 400);
-    // const GMT = 60 * 60 * 1000;
-    // const today = new Date().toISOString().split('T')[0];
-    // const todays = new Date().toLocaleDateString().split('T')
-    // const _startDate = new Date(today).getTime();
-    // const startOfDay = new Date(_startDate - GMT * 5);
-    // const endOfDay = new Date(_startDate - GMT * 29 - 1);
-    // console.log(startOfDay, endOfDay);
-
     const licenses = await prisma.licenses.groupBy({
       by: ['usersId'],
       where: {
-        // startDate: {
-        //   gte: startOfDay,
-        // },
-        // untilDate: {
-        //   lte: endOfDay,
-        // },
         status: 'ACTIVE',
       },
-      // select: {
-      //   // id: true,
-      //   // status: true,
-      //   usersId: true,
-      // },
     });
-    return licenses;
+    const usersActive = await prisma.users.groupBy({
+      by: ['id'],
+      where: {
+        status: true,
+      },
+    });
+    const updatedData = usersActive
+      .filter(item => !licenses.some(exclude => exclude.usersId === item.id))
+      .map(item => ({ usersId: item.id, status: 'PUNTUAL' }));
+
+    const mainData = [
+      ...licenses.map(item => ({ ...item, status: 'PERMISO' })),
+      ...updatedData,
+    ];
+    return { licenses, mainData };
   }
   static async getLicensesByStatus(status: Licenses['status']) {
     const licenses = await prisma.licenses.findMany({
