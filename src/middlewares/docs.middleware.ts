59e5bc053926a2ae5ee7ef04c4e833a6bfe64c72
Jean Ticona <68865099+jeancarlos324@users.noreply.github.com>
2023-12-19 19:11:38 -0500

    add: new documentation on /api-doc route

---
+++ src/middlewares/docs.middleware.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+// import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+
+class Docs {
+  public readonly router: Router;
+  constructor() {
+    this.router = Router();
+    this.setUpRouter();
+  }
+
+  setUpRouter() {
+    const swaggerDocument = YAML.load('./swagger.yaml');
+    this.router.use(
+      '/api-docs',
+      swaggerUi.serve,
+      swaggerUi.setup(swaggerDocument)
+    );
+  }
+}
+
+const { router } = new Docs();
+export default router;
