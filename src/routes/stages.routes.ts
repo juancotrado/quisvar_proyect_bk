c3283d5ab127442078b6b86aa020ae4dcbfd68de
Jean Ticona <68865099+jeancarlos324@users.noreply.github.com>
2024-01-05 01:29:04 -0500

    add: stages details documentation

--- src/routes/stages.routes.ts
+++ src/routes/stages.routes.ts
@@ -4,10 +4,10 @@ import authenticateHandler from '../middlewares/auth.middleware';
 import { _mod_role, _employee_role } from '../middlewares/role.middleware';
 
 const router = Router();
-router.get('/details/:id', StagesControllers.details);
 router.use(authenticateHandler);
 //EMPLOYEE ROLE
 router.use(_employee_role);
+router.get('/details/:id', StagesControllers.details);
 router.get('/', StagesControllers.showList);
 router.get('/:id', StagesControllers.show);
 //MOD ROLE
@@ -15,6 +15,6 @@ router.use(_mod_role);
 router.post('/', StagesControllers.create);
 router.post('/new/:id', addNewStage);
 router.patch('/:id', StagesControllers.update);
-router.patch('/details:id', StagesControllers.updateDetails);
+router.patch('/details/:id', StagesControllers.updateDetails);
 router.delete('/:id', StagesControllers.delete);
 export default router;
