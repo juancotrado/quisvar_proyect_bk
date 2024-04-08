0df2988a995ef169a48ab3fa4a4412061b43c505
Jhon Castillo <gpro1pro@gmail.com>
2024-04-08 01:56:28 -0500

    create profession services to edit, delete and create profession

--- src/routes/profession.routes.ts
+++ src/routes/profession.routes.ts
@@ -2,12 +2,17 @@ import { ProfessionController } from '../controllers/profession.controller';
 import { Router } from 'express';
 
 import authenticateHandler from '../middlewares/auth.middleware';
-// import { _mod_role, _employee_role } from '../middlewares/role.middleware';
+import { role } from '../middlewares';
 
 const professionController = new ProfessionController();
 const router = Router();
 router.use(authenticateHandler);
 //EMPLOYEE ROLE
-router.get('/', professionController.showProfessions);
+router.use(role.RoleHandler(['MOD'], 'centro-de-usuarios'));
+
+router.get('/', professionController.getAll);
+router.post('/', professionController.create);
+router.delete('/:id', professionController.delete);
+router.patch('/:id', professionController.update);
 
 export default router;
--- src/services/profession.services.ts
+++ src/services/profession.services.ts
@@ -1,19 +1,47 @@
 import { Profession } from 'types/profession';
-const fs = require('fs');
+import fs from 'fs';
+import { v4 as uuidv4 } from 'uuid';
 
 class ProfessionService {
-  private _profession: Profession;
+  private professions: Profession[];
 
   constructor() {
-    const json_profession = fs.readFileSync(
-      'public/json/professions.json',
-      'utf-8'
-    );
-    this._profession = JSON.parse(json_profession);
+    const json_profession = fs.readFileSync('json/professions.json', 'utf-8');
+    this.professions = JSON.parse(json_profession);
   }
 
-  showProfessions() {
-    return this._profession;
+  getAll() {
+    return this.professions;
+  }
+
+  create({ abrv, label }: Profession) {
+    const newProfession = {
+      value: uuidv4(),
+      abrv,
+      label,
+    };
+    this.professions.push(newProfession);
+    const json_profession = JSON.stringify(this.professions);
+    fs.writeFileSync('json/professions.json', json_profession, 'utf-8');
+    return newProfession;
+  }
+
+  delete(id: string) {
+    const filterProfession = this.professions.filter(
+      ({ value }) => value !== id
+    );
+    const json_profession = JSON.stringify(filterProfession);
+    fs.writeFileSync('json/professions.json', json_profession, 'utf-8');
+    return this.professions;
+  }
+
+  update({ abrv, label, value }: Profession) {
+    const updateProfession = this.professions.map(profession =>
+      profession.value === value ? { ...profession, abrv, label } : profession
+    );
+    const json_profession = JSON.stringify(updateProfession);
+    fs.writeFileSync('json/professions.json', json_profession, 'utf-8');
+    return updateProfession;
   }
 }
 
