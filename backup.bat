75f55375cb42152f886fbe6c5c37188d09d01d7e
jeanticona <jecar324@gmail.com>
2024-06-03 21:00:18 -0500

    fix(server): changed backup cmd

--- backup.bat
+++ backup.bat
@@ -4,12 +4,8 @@ set NAME_BACKUP= ""
 set OUTPUT_BACKUP_FILE=E:/proyecto_quisvar/quisvar_proyect_bk/Backup
 docker exec -it %DATABASE_CONTAINER% bash -c "pg_dump -U dbtaskmanager -d dbtaskmanager > backup2.sql"
 docker exec -it %DATABASE_CONTAINER% bash -c "ls"
-timeout /t 15 >nul
 if not exist "%OUTPUT_BACKUP_FILE%" (
     mkdir "%OUTPUT_BACKUP_FILE%"
-    echo La carpeta se ha creado correctamente.
-) else (
-    echo La carpeta ya existe.
 )
 
 for /f "tokens=1-3 delims=/" %%a in ('date /t') do (
